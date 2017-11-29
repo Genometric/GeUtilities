838a960c92da3807d1512e1b83cbf95e20779d32
VJalili <jalili.vahid@gmail.com>
2017-11-28 21:48:52 -0800

    Added IGenomics.

---
+++ IGenomics/Intervals/IGeneralFeature.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+namespace Genometric.GeUtilities.IGenomics
+{
+    public interface IGeneralFeature : IExtendedMetadata
+    {
+        byte Feature { set; get; }
+        string Attribute { set; get; }
+    }
+}
---
+++ IGenomics/Intervals/IMetadata.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+namespace Genometric.GeUtilities.IGenomics
+{
+    public interface IMetaData
+    {
+    }
+}
