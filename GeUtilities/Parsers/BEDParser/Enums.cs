858a8bec715989513c1310078a4dc42bc94e0e5c
VJalili <jalili.vahid@gmail.com>
2017-12-03 08:26:43 -0800

    Changed the file hierarchy of parsers.

--- GeUtilities/Parsers/BEDParser/Enums.cs
+++
@@ -1,8 +0,0 @@
-﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
-// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
-// See the LICENSE file in the project root for more information.
-
-namespace Genometric.GeUtilities.Parsers
-{
-    public enum PValueFormat {SameAsInput, minus1_Log10_pValue, minus10_Log10_pValue, minus100_Log10_pValue };
-}
