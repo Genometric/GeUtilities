d1cd7a26be71a9229b39ef8dd25f2c660c0b135a
VJalili <jalili.vahid@gmail.com>
2017-12-25 11:56:15 -0800

    Added BED column order tests.

---
+++ GeUtilities.Tests/BEDParserTests/TestColumnOrders.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+using Genometric.GeUtilities.IntervalBasedDataParsers.Model.Defaults;
+using Genometric.GeUtilities.Parsers;
+using System;
+using Xunit;
+
+namespace GeUtilities.Tests
+{
+    public class TestColumnOrders
+    {
+        private string _chr = "chr1";
+
+        [Fact]
+        public void TestDefaultBEDColumnOrder()
+        {
+            using (TestBEDFileCreator testFile = new TestBEDFileCreator(chr: _chr))
+            {
+                BEDParser<ChIPSeqPeak> bedParser = new BEDParser<ChIPSeqPeak>(testFile.TestFilePath, dropPeakIfInvalidValue: true);
+                var parsedData = bedParser.Parse();
+
+                Assert.False(parsedData.Chromosomes.ContainsKey(_chr));
+            }
+        }
+    }
+}
