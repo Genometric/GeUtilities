ff6088ac17ada2548e61b258adfad80fa727dad3
VJalili <jalili.vahid@gmail.com>
2017-12-26 20:16:28 -0800

    Added one more number of p-value stats tests.

--- GeUtilities.Tests/StatsTests/TestBEDStats.cs
+++ GeUtilities.Tests/StatsTests/TestBEDStats.cs
@@ -4,6 +4,7 @@
 
 using Genometric.GeUtilities.IntervalBasedDataParsers.Model.Defaults;
 using Genometric.GeUtilities.Parsers;
+using System;
 using Xunit;
 
 namespace GeUtilities.Tests
@@ -28,7 +29,7 @@ namespace GeUtilities.Tests
 
         [Theory]
         [InlineData(new double[] { 0.001 }, 0.001)]
-        [InlineData(new double[] { 0.1, 0.001, 0.0001}, 0.1)]
+        [InlineData(new double[] { 0.1, 0.01, 0.001, 0.0001 }, 0.1)]
         public void TestPValueHighest(double[] pValues, double pValueHighest)
         {
             var stats = new BEDStats();
@@ -40,7 +41,7 @@ namespace GeUtilities.Tests
 
         [Theory]
         [InlineData(new double[] { 0.001 }, 0.001)]
-        [InlineData(new double[] { 0.1, 0.001, 0.0001 }, 0.0001)]
+        [InlineData(new double[] { 0.1, 0.01, 0.001, 0.0001 }, 0.0001)]
         public void TestPValueLowest(double[] pValues, double pValueLowest)
         {
             var stats = new BEDStats();
@@ -52,7 +53,7 @@ namespace GeUtilities.Tests
 
         [Theory]
         [InlineData(new double[] { 0.001 }, 0.001)]
-        [InlineData(new double[] { 0.1, 0.001, 0.0001 }, 0.0337)]
+        [InlineData(new double[] { 0.1, 0.01, 0.001, 0.0001 }, 0.027775)]
         public void TestPValueMean(double[] pValues, double mean)
         {
             var stats = new BEDStats();
@@ -64,14 +65,14 @@ namespace GeUtilities.Tests
 
         [Theory]
         [InlineData(new double[] { 0.001 }, 0.0)]
-        [InlineData(new double[] { 0.1, 0.001, 0.0001 }, 0.046882619)]
+        [InlineData(new double[] { 0.1, 0.01, 0.001, 0.0001 }, 0.041878418)]
         public void TestPValueSTDV(double[] pValues, double stdv)
         {
             var stats = new BEDStats();
             foreach (var peak in CreatePeaks(pValues))
                 stats.Update(peak);
 
-            Assert.True(stats.PValueSTDV == stdv);
+            Assert.True(Math.Round(stats.PValueSTDV, 9) == stdv);
         }
     }
 }
