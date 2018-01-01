d75c93aa30cc4cf061f27e1d89bc02bba7b3f6a7
VJalili <jalili.vahid@gmail.com>
2018-01-01 03:06:33 -0800

    Updated the BEDParser's concrete class tests.

--- GeUtilities.Tests/BEDParser/ConcreteClassArguments.cs
+++ GeUtilities.Tests/BEDParser/ConcreteClassArguments.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using Genometric.GeUtilities.Parsers;
+using Genometric.GeUtilities.ReferenceGenomes;
 using Xunit;
 
 /// <summary>
@@ -12,62 +13,8 @@ namespace GeUtilities.Tests.TBEDParser
 {
     public class ConcreteClassArguments
     {
-        [Theory]
-        [InlineData("chr1")]
-        [InlineData("Chr1")]
-        [InlineData("chr10")]
-        [InlineData("Chr10")]
-        [InlineData("chrX")]
-        [InlineData("ChrX")]
-        public void ReadChr(string chr)
-        {
-            // Arrange
-            using (TempFileCreator testFile = new TempFileCreator(new Columns(chr: chr)))
-            {
-                // Act
-                BEDParser parser = new BEDParser(testFile.TempFilePath);
-                var parsedData = parser.Parse();
-
-                // Assert
-                Assert.True(parsedData.Chromosomes.ContainsKey(chr));
-            }
-        }
-
-        [Theory]
-        [InlineData("chr2")]
-        [InlineData("chrX")]
-        public void FailReadChr(string chr)
-        {
-            // Arrange
-            using (TempFileCreator testFile = new TempFileCreator(new Columns(chr: "chr1")))
-            {
-                // Act
-                BEDParser parser = new BEDParser(testFile.TempFilePath);
-                var parsedData = parser.Parse();
-
-                // Assert
-                Assert.False(parsedData.Chromosomes.ContainsKey(chr));
-            }
-        }
-
         [Fact]
-        public void ReadStrand()
-        {
-            // Arrange
-            var columns = new Columns();
-            using (TempFileCreator testFile = new TempFileCreator(columns))
-            {
-                // ACt
-                BEDParser parser = new BEDParser(testFile.TempFilePath);
-                var parsedData = parser.Parse();
-
-                // Assert
-                Assert.True(parsedData.Chromosomes[columns.Chr].Strands.ContainsKey(columns.Strand));
-            }
-        }
-
-        [Fact]
-        public void ReadLeft()
+        public void AllDefaultArguments()
         {
             // Arrange
             var columns = new Columns();
@@ -78,115 +25,64 @@ namespace GeUtilities.Tests.TBEDParser
                 var parsedData = parser.Parse();
 
                 // Assert
-                Assert.True(parsedData.Chromosomes[columns.Chr].Strands[columns.Strand].Intervals[0].Left == columns.Peak.Left);
+                Assert.True(parsedData.Chromosomes.ContainsKey(columns.Chr));
             }
         }
 
         [Fact]
-        public void FailReadLeft()
-        {
-            // Arrange
-            using (TempFileCreator testFile = new TempFileCreator("chr1\t10V\t20\tGeUtilities_01\t123.4"))
-            {
-                // Act
-                BEDParser parser = new BEDParser(testFile.TempFilePath);
-                var parsedData = parser.Parse();
-
-                // Assert
-                Assert.False(parsedData.Chromosomes.ContainsKey("chr1"));
-            }
-        }
-
-        [Fact]
-        public void ReadRight()
+        public void PartiallySetArguments()
         {
             // Arrange
             var columns = new Columns();
             using (TempFileCreator testFile = new TempFileCreator(columns))
             {
                 // Act
-                BEDParser parser = new BEDParser(testFile.TempFilePath);
+                BEDParser parser = new BEDParser(
+                    testFile.TempFilePath,
+                    assembly: Assemblies.hg19,
+                    defaultValue: 1E-8,
+                    pValueFormat: PValueFormat.SameAsInput,
+                    dropPeakIfInvalidValue: true,
+                    startOffset: 0,
+                    readOnlyValidChrs: true,
+                    maxLinesToBeRead: 1,
+                    hashFunction: HashFunction.FNV);
                 var parsedData = parser.Parse();
 
                 // Assert
-                Assert.True(parsedData.Chromosomes[columns.Chr].Strands[columns.Strand].Intervals[0].Right == columns.Peak.Right);
+                Assert.True(parsedData.Chromosomes.ContainsKey(columns.Chr));
             }
         }
 
         [Fact]
-        public void FailReadRightInvalidValue()
-        {
-            // Arrange
-            using (TempFileCreator testFile = new TempFileCreator("chr1\t10\t20V\tGeUtilities_01\t123.4"))
-            {
-                // Act
-                BEDParser parser = new BEDParser(testFile.TempFilePath);
-                var parsedData = parser.Parse();
-
-                // Assert
-                Assert.False(parsedData.Chromosomes.ContainsKey("chr1"));
-            }
-        }
-
-        [Fact]
-        public void FailReadRightColumnIndexOutOfRange()
-        {
-            // Arrange
-            using (TempFileCreator testFile = new TempFileCreator("chr1\t10\t20\tGeUtilities_01\t123.4"))
-            {
-                // Act
-                BEDParser parser = new BEDParser(testFile.TempFilePath, 0, 1, 10, 3, 4);
-                var parsedData = parser.Parse();
-
-                // Assert
-                Assert.False(parsedData.Chromosomes.ContainsKey("chr1"));
-            }
-        }
-
-        [Fact]
-        public void ReadName()
+        public void FullySetArguments()
         {
             // Arrange
             var columns = new Columns();
             using (TempFileCreator testFile = new TempFileCreator(columns))
             {
                 // Act
-                BEDParser parser = new BEDParser(testFile.TempFilePath);
+                BEDParser parser = new BEDParser(
+                    testFile.TempFilePath,
+                    chrColumn: columns.ChrColumn,
+                    leftEndColumn: columns.LeftColumn,
+                    rightEndColumn: columns.RightColumn,
+                    nameColumn: columns.NameColumn,
+                    valueColumn: columns.ValueColumn,
+                    strandColumn: columns.StrandColumn,
+                    summitColumn: columns.SummitColumn,
+                    assembly: Assemblies.hg19,
+                    defaultValue: 1E-8,
+                    pValueFormat: PValueFormat.SameAsInput,
+                    dropPeakIfInvalidValue: true,
+                    startOffset: 0,
+                    readOnlyValidChrs: true,
+                    maxLinesToBeRead: 1,
+                    hashFunction: HashFunction.FNV);
                 var parsedData = parser.Parse();
 
                 // Assert
-                Assert.True(parsedData.Chromosomes[columns.Chr].Strands[columns.Strand].Intervals[0].Name == columns.Peak.Name);
-            }
-        }
-
-        [Fact]
-        public void ReadValue()
-        {
-            // Arrange
-            var columns = new Columns();
-            using (TempFileCreator testFile = new TempFileCreator(columns))
-            {
-                // Act
-                BEDParser parser = new BEDParser(testFile.TempFilePath);
-                var parsedData = parser.Parse();
-
-                // Assert
-                Assert.True(parsedData.Chromosomes[columns.Chr].Strands[columns.Strand].Intervals[0].Value == columns.Peak.Value);
-            }
-        }
-
-        [Fact]
-        public void FailReadValue()
-        {
-            // Arrange
-            using (TempFileCreator testFile = new TempFileCreator("chr1\t10\t20\tGeUtilities_01\t123..45"))
-            {
-                // Act
-                BEDParser parser = new BEDParser(testFile.TempFilePath);
-                var parsedData = parser.Parse();
-
-                // Assert
-                Assert.False(parsedData.Chromosomes.ContainsKey("chr1"));
+                Assert.True(parsedData.Chromosomes.ContainsKey(columns.Chr));
             }
         }
     }
