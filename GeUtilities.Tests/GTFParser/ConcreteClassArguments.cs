a1f3cb60116379ae444711a05ad384769f156b8a
VJalili <jalili.vahid@gmail.com>
2018-02-01 23:36:17 -0800

    Removed unused references.

--- GeUtilities.Tests/GTFParser/ConcreteClassArguments.cs
+++ GeUtilities.Tests/GTFParser/ConcreteClassArguments.cs
@@ -2,9 +2,7 @@
 // The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
 // See the LICENSE file in the project root for more information.
 
-using Genometric.GeUtilities.IntervalBasedDataParsers.Model.Defaults;
 using Genometric.GeUtilities.Parsers;
-using Genometric.GeUtilities.ReferenceGenomes;
 using Xunit;
 
 namespace GeUtilities.Tests.TGTFParser
--- GeUtilities.Tests/RefSeqParser/ConcreteClassArguments.cs
+++ GeUtilities.Tests/RefSeqParser/ConcreteClassArguments.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using Genometric.GeUtilities.Parsers;
-using Genometric.GeUtilities.ReferenceGenomes;
 using Xunit;
 
 namespace GeUtilities.Tests.TRefSeqParser
