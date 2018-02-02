3988121c70026d35deaec7670d883b1a10a6d20b
VJalili <jalili.vahid@gmail.com>
2018-02-01 22:37:21 -0800

    Removed unused using.

--- GeUtilities/IntervalBasedDataParsers/BEDParser/BEDParserGeneric.cs
+++ GeUtilities/IntervalBasedDataParsers/BEDParser/BEDParserGeneric.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using Genometric.GeUtilities.IGenomics;
-using Genometric.GeUtilities.ReferenceGenomes;
 using System;
 
 namespace Genometric.GeUtilities.Parsers
