2c8d8aec00482916e76f5974817f6f5d93f8122a
VJalili <jalili.vahid@gmail.com>
2018-02-06 00:50:39 -0800

    Update RefSeq parser temp file creator to avoid double-displosal of file stream.

--- GeUtilities.Tests/RefSeqParser/TempFileCreator.cs
+++ GeUtilities.Tests/RefSeqParser/TempFileCreator.cs
@@ -9,28 +9,46 @@ namespace GeUtilities.Tests.TRefSeqParser
 {
     public class TempFileCreator : IDisposable
     {
-        private string _tempFilePath;
+        private readonly string _tempFilePath;
         public string TempFilePath { get { return _tempFilePath; } }
 
         public TempFileCreator(string line)
         {
-            _tempFilePath = Path.GetTempPath() + Guid.NewGuid().ToString() + ".refSeq";
-            using (FileStream fs = File.Create(_tempFilePath))
-            using (StreamWriter sw = new StreamWriter(fs))
-                sw.WriteLine(line);
+            FileStream fs = null;
+            try
+            {
+                _tempFilePath = Path.GetTempPath() + Guid.NewGuid().ToString() + ".refSeq";
+                fs = File.Create(_tempFilePath);
+                using (StreamWriter sw = new StreamWriter(fs))
+                    sw.WriteLine(line);
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Dispose();
+            }
         }
 
         public TempFileCreator(Columns columns, int headerLineCount = 0, int genesCount = 1)
         {
-            _tempFilePath = Path.GetTempPath() + Guid.NewGuid().ToString() + ".refSeq";
-            using (FileStream fs = File.Create(_tempFilePath))
-            using (StreamWriter sw = new StreamWriter(fs))
+            FileStream fs = null;
+            try
             {
-                while (headerLineCount-- > 0)
-                    sw.WriteLine(columns.GetSampleHeader());
+                _tempFilePath = Path.GetTempPath() + Guid.NewGuid().ToString() + ".refSeq";
+                fs = File.Create(_tempFilePath);
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    while (headerLineCount-- > 0)
+                        sw.WriteLine(columns.GetSampleHeader());
 
-                while (genesCount-- > 0)
-                    sw.WriteLine(columns.GetSampleLine());
+                    while (genesCount-- > 0)
+                        sw.WriteLine(columns.GetSampleLine());
+                }
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Dispose();
             }
         }
 
