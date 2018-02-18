f2a7c4c0b8906a6e8dc2049ed46c22098126e5e0
VJalili <jalili.vahid@gmail.com>
2018-02-18 01:03:37 -0800

    Add GTF and RefSeq columns classes.

---
+++ GeUtilities/IntervalBasedDataParsers/Model/Columns/RefSeqColumns.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+namespace Genometric.GeUtilities.IntervalParsers
+{
+    public class RefSeqColumns : BaseColumns
+    {
+        public byte RefSeqID { set; get; }
+        public byte GeneSeymbol { set; get; }
+    }
+}
