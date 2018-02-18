42a9870551761dd4c812583cf123226721dc034c
VJalili <jalili.vahid@gmail.com>
2018-02-18 00:51:30 -0800

    Created Base and BED columns classes.

---
+++ GeUtilities/IntervalBasedDataParsers/Model/Columns/BEDColumns.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+namespace Genometric.GeUtilities.IntervalParsers
+{
+    public class BEDColumns : BaseColumns
+    {
+        public byte Name { set; get; }
+        public byte Value { set; get; }
+        public sbyte Summit { set; get; }
+
+        public BEDColumns()
+        {
+            Summit = -1;
+        }
+    }
+}
---
+++ GeUtilities/IntervalBasedDataParsers/Model/Columns/BaseColumns.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+namespace Genometric.GeUtilities.IntervalParsers
+{
+    public abstract class BaseColumns
+    {
+        public byte Chr { set; get; }
+        public byte Left { set; get; }
+        public sbyte Right { set; get; }
+        public sbyte Strand { set; get; }
+
+        protected BaseColumns()
+        {
+            Strand = -1;
+        }
+    }
+}
