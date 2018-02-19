c5bfa7be3093fc2110f27bd39befbd2c47e830fd
VJalili <jalili.vahid@gmail.com>
2018-02-18 20:59:17 -0800

    Refactor the name space of parsers test functions removing `T` prefix and `Parser` postfix.

---
+++ GeUtilities.Tests/IntervalParsers/Model/Defaults/TestVCF.cs
@@ -0,0 +1,137 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+using Genometric.GeUtilities.IGenomics;
+using Genometric.GeUtilities.IntervalParsers.Model.Defaults;
+using System;
+using Xunit;
+
+namespace GeUtilities.Tests.IntervalParsers.ModelTests.Defaults
+{
+    public class TestVCF
+    {
+        internal static Variant GetTempVCF()
+        {
+            return new Variant()
+            {
+                Left = 10,
+                ID = "ID",
+                RefBase = ConvertStringToBasePair("ACGN"),
+                AltBase = ConvertStringToBasePair("UGCA"),
+                Quality = 123.4,
+                Filter = "Filter",
+                Info = "Info"
+            };
+        }
+
+        private static Base[] ConvertStringToBasePair(string input)
+        {
+            var rtv = new Base[input.Length];
+            for (int i = 0; i < input.Length; i++)
+                switch (input[i])
+                {
+                    case 'A': rtv[i] = Base.A; break;
+                    case 'C': rtv[i] = Base.C; break;
+                    case 'G': rtv[i] = Base.G; break;
+                    case 'N': rtv[i] = Base.N; break;
+                    case 'T': rtv[i] = Base.T; break;
+                    default: rtv[i] = Base.U; break;
+                }
+            return rtv;
+        }
+
+        [Theory]
+        [InlineData(0, 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info", 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info")]
+        [InlineData(-1, 8, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info", 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info")]
+        [InlineData(-1, 10, "I", "ACGT", "TGCA", 123.4, "Filter", "Info", 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info")]
+        [InlineData(-1, 10, "ID", "A", "TGCA", 123.4, "Filter", "Info", 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info")]
+        [InlineData(-1, 10, "ID", "ACGT", "A", 123.4, "Filter", "Info", 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info")]
+        [InlineData(-1, 10, "ID", "ACGT", "TGCA", 23.4, "Filter", "Info", 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info")]
+        [InlineData(-1, 10, "ID", "ACGT", "TGCA", 123.4, "F", "Info", 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info")]
+        [InlineData(-1, 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "I", 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info")]
+        [InlineData(1, 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info", 8, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info")]
+        [InlineData(1, 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info", 10, "I", "ACGT", "TGCA", 123.4, "Filter", "Info")]
+        [InlineData(1, 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info", 10, "ID", "A", "TGCA", 123.4, "Filter", "Info")]
+        [InlineData(1, 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info", 10, "ID", "ACGT", "A", 123.4, "Filter", "Info")]
+        [InlineData(1, 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info", 10, "ID", "ACGT", "TGCA", 23.4, "Filter", "Info")]
+        [InlineData(1, 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info", 10, "ID", "ACGT", "TGCA", 123.4, "F", "Info")]
+        [InlineData(1, 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "Info", 10, "ID", "ACGT", "TGCA", 123.4, "Filter", "I")]
+        public void ComparisonTest(
+            int comparisonResult,
+            int aLeft, string aID, string aRefbp, string aAltbp, double aQuality, string aFilter, string aInfo,
+            int bLeft, string bID, string bRefbp, string bAltbp, double bQuality, string bFilter, string bInfo)
+        {
+            // Arrange
+            var aVariant = new Variant()
+            {
+                Left = aLeft,
+                ID = aID,
+                RefBase = ConvertStringToBasePair(aRefbp),
+                AltBase = ConvertStringToBasePair(aAltbp),
+                Quality = aQuality,
+                Filter = aFilter,
+                Info = aInfo
+            };
+
+            var bVariant = new Variant()
+            {
+                Left = bLeft,
+                ID = bID,
+                RefBase = ConvertStringToBasePair(bRefbp),
+                AltBase = ConvertStringToBasePair(bAltbp),
+                Quality = bQuality,
+                Filter = bFilter,
+                Info = bInfo
+            };
+
+            // Act & Assert
+            Assert.True(aVariant.CompareTo(bVariant) == comparisonResult);
+        }
+
+        [Fact]
+        public void ComparisonTestWithNullObject()
+        {
+            // Arrange
+            var variant = GetTempVCF();
+
+            // Act & Assert
+            Assert.True(variant.CompareTo(null) == 1);
+        }
+
+        [Fact]
+        public void ComparisonTestWithNullObject2()
+        {
+            // Arrange
+            var variant = GetTempVCF();
+
+            // Act & Assert
+            Assert.True(variant.CompareTo((object)null) == 1);
+        }
+
+        [Fact]
+        public void ComparisonTestWithAPeakAsObject()
+        {
+            // Arrange
+            var aVariant = GetTempVCF();
+            var bVariant = GetTempVCF();
+
+            // Act & Assert
+            Assert.True(aVariant.CompareTo((object)bVariant) == 0);
+        }
+
+        [Fact]
+        public void CheckNotImplementedComparison()
+        {
+            // Arrange
+            var aVariant = GetTempVCF();
+            var aPeak = TestChIPSeqPeak.GetTempChIPSeqPeak();
+
+            // Act & Assert
+            Exception exception = Assert.Throws<NotImplementedException>(() => aVariant.CompareTo(aPeak));
+
+            // Act & Assert
+            Assert.Equal("Comparison with other object types is not implemented.", exception.Message);
+        }
+    }
+}
---
+++ GeUtilities.Tests/IntervalParsers/Model/ParsedIntervals.cs
@@ -0,0 +1,86 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+using Genometric.GeUtilities.IntervalParsers;
+using Genometric.GeUtilities.IntervalParsers.Model.Defaults;
+using Genometric.GeUtilities.ReferenceGenomes;
+using GeUtilities.Tests.IntervalParsers.BED;
+using System.IO;
+using Xunit;
+
+namespace GeUtilities.Tests.IntervalParsers.ModelTests
+{
+    public class ParsedIntervals
+    {
+        [Fact]
+        public void TestFileHashKey()
+        {
+            // Arrange
+            string peak = "chr1\t10\t20\tName\t100.0";
+            using (var testFile = new TempFileCreator(peak))
+            {
+                // Act
+                var parser = new BEDParser<ChIPSeqPeak>(testFile.TempFilePath);
+                var parsedBED = parser.Parse();
+
+                // Assert
+                Assert.True(parsedBED.FileHashKey != 0);
+            }
+        }
+
+        [Fact]
+        public void TestFileName()
+        {
+            // Arrange
+            string peak = "chr1\t10\t20\tName\t100.0";
+            using (var testFile = new TempFileCreator(peak))
+            {
+                // Act
+                var parser = new BEDParser<ChIPSeqPeak>(testFile.TempFilePath);
+                var parsedBED = parser.Parse();
+
+                // Assert
+                Assert.True(parsedBED.FileName == Path.GetFileName(testFile.TempFilePath));
+            }
+        }
+
+        [Fact]
+        public void TestFilePath()
+        {
+            // Arrange
+            string peak = "chr1\t10\t20\tName\t100.0";
+            using (var testFile = new TempFileCreator(peak))
+            {
+                // Act
+                var parser = new BEDParser<ChIPSeqPeak>(testFile.TempFilePath);
+                var parsedBED = parser.Parse();
+
+                // Assert
+                Assert.True(parsedBED.FilePath == Path.GetFullPath(testFile.TempFilePath));
+            }
+        }
+
+        [Theory]
+        [InlineData(Assemblies.hg19)]
+        [InlineData(Assemblies.mm10)]
+        [InlineData(Assemblies.Unknown)]
+        public void TestAssembly(Assemblies assembly)
+        {
+            // Arrange
+            string peak = "chr1\t10\t20\tName\t100.0";
+            using (var testFile = new TempFileCreator(peak))
+            {
+                // Act
+                var parser = new BEDParser<ChIPSeqPeak>(testFile.TempFilePath)
+                {
+                    Assembly = assembly
+                };
+                var parsedBED = parser.Parse();
+
+                // Assert
+                Assert.True(parsedBED.Assembly == assembly);
+            }
+        }
+    }
+}
