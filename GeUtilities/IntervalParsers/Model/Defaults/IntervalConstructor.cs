80dc1826a3e1f79afbcea7df8ef5fafcc97cf563
VJalili <jalili.vahid@gmail.com>
2018-09-06 23:19:30 -0700

    Implement a constructor for Interval class.

---
+++ GeUtilities/IntervalParsers/Model/Defaults/IntervalConstructor.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+using Genometric.GeUtilities.IGenomics;
+
+namespace Genometric.GeUtilities.IntervalParsers.Model.Defaults
+{
+    public class IntervalConstructor : IIntervalConstructor<Interval>
+    {
+        public Interval Construct(int left, int right, string hashSeed = "")
+        {
+            return new Interval(left, right, hashSeed);
+        }
+    }
+}
