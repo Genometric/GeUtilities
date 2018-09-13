d54f2293084a1537f07250a9c477f828bd18e295
VJalili <jalili.vahid@gmail.com>
2018-09-13 00:05:55 -0700

    Use string instead of String.

--- GeUtilities.Tests/Intervals/Parsers/BED/Features.cs
+++ GeUtilities.Tests/Intervals/Parsers/BED/Features.cs
@@ -26,7 +26,7 @@ namespace Genometric.GeUtilities.Tests.Intervals.Parsers.Bed
             Exception exception = Assert.Throws<FileNotFoundException>(() => parser.Parse(fileName));
 
             // Assert
-            Assert.False(String.IsNullOrEmpty(exception.Message));
+            Assert.False(string.IsNullOrEmpty(exception.Message));
             Assert.Equal(string.Format("The file `{0}` does not exist or is inaccessible.", fileName), exception.Message);
         }
 
